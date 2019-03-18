26232fbae52ac681e2dc974cc760eb7bac2eac9c
Isaac Massot <isaac.mg@coditramuntana.com>
2019-03-18 14:53:24 +0100

    upgrade to 0.16 stable and add verification module

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -5,5 +5,6 @@ keys = %w[SECRET_KEY_BASE]
 unless env.development? || env.test?
   keys += %w[DB_DATABASE DB_PASSWORD DB_USERNAME]
   keys += %w[MAILER_SMTP_ADDRESS MAILER_SMTP_DOMAIN MAILER_SMTP_PORT MAILER_SMTP_USER_NAME MAILER_SMTP_PASSWORD]
+  keys += %w[SITGES_CENSUS_URL SITGES_CENSUS_SECRET]
 end
 Figaro.require_keys(keys)
